7961dd854fc4c29f9753762151b9a7808e4400cb
CleuJunior <cleu.junior@gmail.com>
2023-11-13 17:01:25 -0300

    feat: Healthy User Microservice

---
+++ ms-healthy-user/src/main/java/br/com/cleonildo/MsHealthyUserApplication.java
@@ -0,0 +1,13 @@
+package br.com.cleonildo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsHealthyUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsHealthyUserApplication.class, args);
+	}
+
+}
---
+++ ms-healthy-user/src/test/java/br/com/cleonildo/MsHealthyUserApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cleonildo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsHealthyUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
